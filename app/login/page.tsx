335776280b816bfefc78707f269873fcfafabb29
gctoledo <gabrielcostatoledo@outlook.com>
2025-06-27 13:42:48 -0300

    chore: update project name

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -17,16 +17,17 @@ const LoginPage = async () => {
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
         <Image
           src={"/logo.svg"}
-          alt="Finance AI"
+          alt="Financez"
           width={173}
           height={39}
           className="mb-8"
         />
         <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
-        <p className="text-muted-foreground mb-8">
-          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
-          monitorar suas movimentações, e oferecer insights personalizados,
-          facilitando o controle do seu orçamento.
+        <p className="mb-8 text-muted-foreground">
+          A Financez é uma plataforma de gestão financeira que facilita sua
+          organização financeira utilizando IA para monitorar suas
+          movimentações, e oferecer insights personalizados, deixando você no
+          controle do seu orçamento.
         </p>
 
         <SignInButton>
