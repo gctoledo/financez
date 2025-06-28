eae8872db3e93b484c9be60a36185967f3b42ff4
gctoledo <gabrielcostatoledo@outlook.com>
2025-06-28 02:57:44 -0300

    feat: add summary cards

---
+++ app/(home)/_components/summary-cards.tsx
@@ -0,0 +1,98 @@
+import {
+  PiggyBankIcon,
+  TrendingDownIcon,
+  TrendingUpIcon,
+  WalletIcon,
+} from "lucide-react";
+import SummaryCard from "./symmary-card";
+import { db } from "@/app/_lib/prisma";
+
+interface SummaryCardsProps {
+  month: string;
+}
+
+const SummaryCards = async ({ month }: SummaryCardsProps) => {
+  const actDate = new Date();
+
+  const where = {
+    date: {
+      gte: new Date(`${actDate.getFullYear()}-${month}-01`),
+      lte: new Date(`${actDate.getFullYear()}-${month}-31`),
+    },
+  };
+
+  const investmentsTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: {
+          ...where,
+          type: "INVESTMENT",
+        },
+        _sum: {
+          amount: true,
+        },
+      })
+    )._sum.amount,
+  );
+
+  const expensesTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: {
+          ...where,
+          type: "EXPENSE",
+        },
+        _sum: {
+          amount: true,
+        },
+      })
+    )._sum.amount,
+  );
+
+  const depositsTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: {
+          ...where,
+          type: "DEPOSIT",
+        },
+        _sum: {
+          amount: true,
+        },
+      })
+    )._sum.amount,
+  );
+
+  const balance = depositsTotal - investmentsTotal - expensesTotal;
+
+  return (
+    <div className="space-y-6">
+      <SummaryCard
+        icon={<WalletIcon size={16} />}
+        title="Saldo"
+        amount={balance}
+        size="large"
+      />
+
+      <div className="grid grid-cols-3 gap-6">
+        <SummaryCard
+          icon={<PiggyBankIcon size={16} />}
+          title="Investido"
+          amount={investmentsTotal}
+        />
+        <SummaryCard
+          icon={<TrendingUpIcon size={16} className="text-primary" />}
+          title="Receita"
+          amount={depositsTotal}
+        />
+        <SummaryCard
+          icon={<TrendingDownIcon size={16} className="text-danger" />}
+          title="Despesa"
+          amount={expensesTotal}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SummaryCards;
