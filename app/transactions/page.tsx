80187e8fdc03b6d6db9726757afd9d93ae1c019e
gctoledo <gabrielcostatoledo@outlook.com>
2025-06-28 02:10:24 -0300

    feat: ensure transactions and subscriptions pages are protected

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,9 +3,22 @@ import { DataTable } from "../_components/ui/data-table";
 import AddTransactionButton from "../_components/add-transaction-button";
 import { transactionColumns } from "../transactions/_columns";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const TransactionsPage = async () => {
-  const transactions = await db.transaction.findMany({});
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <>
       <Navbar />
