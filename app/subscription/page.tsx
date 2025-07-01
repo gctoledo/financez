efc3980bc3ba4b96c5ad8015994ada112de60cad
gctoledo <gabrielcostatoledo@outlook.com>
2025-07-01 14:59:23 -0300

    feat: add responsive layout to subscription page

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -25,8 +25,8 @@ const SubscriptionPage = async () => {
       <div className="space-y-6 p-6">
         <h1 className="text-2xl font-bold">Assinatura</h1>
 
-        <div className="flex gap-6">
-          <Card className="w-[450px]">
+        <div className="flex flex-col gap-6 md:grid md:grid-cols-2 md:justify-center lg:flex lg:flex-row lg:justify-normal">
+          <Card className="w-full md:w-full lg:w-[450px]">
             <CardHeader className="border-b border-solid py-8">
               <h2 className="text-center text-2xl font-semibold">
                 Plano Básico
@@ -54,10 +54,10 @@ const SubscriptionPage = async () => {
             </CardContent>
           </Card>
 
-          <Card className="w-[450px]">
+          <Card className="w-full md:w-full lg:w-[450px]">
             <CardHeader className="relative border-b border-solid py-8">
               {hasPremiumPlan && (
-                <Badge className="absolute left-4 top-12 bg-primary/10 text-primary hover:text-white">
+                <Badge className="absolute left-4 top-4 bg-primary/10 text-primary hover:text-white md:top-12">
                   Ativo
                 </Badge>
               )}
